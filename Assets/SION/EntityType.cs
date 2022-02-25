447efc8e32350470659236beb4429299f99a66d1
Ian Horswill <ian@northwestern.edu>
2022-02-25 15:55:40 -0600

    Refactored to move ListUtilities into main Step assembly so it could be used for the RandomElement primitive

--- Assets/SION/EntityType.cs
+++ Assets/SION/EntityType.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Step.Interpreter;
+using Step.Utilities;
 
 namespace Assets.SION
 {
--- Assets/SION/RelationStore.cs
+++ Assets/SION/RelationStore.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Step.Interpreter;
+using Step.Utilities;
 
 namespace Assets.SION
 {
